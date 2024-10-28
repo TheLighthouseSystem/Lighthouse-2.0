e13da0efbb602e14c001bc1657f60afe2471770e
DannyLiehr <dannyisyelling@gmail.com>
2024-10-27 20:15:48 -0400

    First commit.

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// API endpoint
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+});
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/src', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
